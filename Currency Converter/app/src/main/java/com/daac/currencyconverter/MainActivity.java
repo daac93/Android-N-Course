ff4903501ac90ff713308f7b603a718ebf242b82
Daniel Arguedas <daac93@gmail.com>
2018-01-16 15:59:05 -0600

    Add currency converting functionality (USD to CRC)

--- Currency Converter/app/src/main/java/com/daac/currencyconverter/MainActivity.java
+++ Currency Converter/app/src/main/java/com/daac/currencyconverter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.daac.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convertCurrency(View view)  {
+        EditText amount = findViewById(R.id.currencyAmount);
+
+        Double amountAsDouble = Double.parseDouble(amount.getText().toString());
+
+        amountAsDouble = amountAsDouble * 570;
+
+        Toast.makeText(this, amountAsDouble.toString(), Toast.LENGTH_LONG).show();
     }
 }
