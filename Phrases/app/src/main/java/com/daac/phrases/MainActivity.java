47c879eab43e4211378a2893a68ca0a8d2ca9e4f
Daniel Arguedas <d.arguedas.calderon@accenture.com>
2018-01-19 15:24:47 -0600

    Improve id resolution of audio file

--- Phrases/app/src/main/java/com/daac/phrases/MainActivity.java
+++ Phrases/app/src/main/java/com/daac/phrases/MainActivity.java
@@ -10,8 +10,6 @@ import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaPlayer mediaPlayer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,20 +20,11 @@ public class MainActivity extends AppCompatActivity {
         String audioFile = (String) view.getTag();
         int resourceId = -1;
 
-        Class raw = R.raw.class;
-        Field[] fields = raw.getFields();
-        for(Field field : fields)   {
-            try {
-                if (field.getName().equals(audioFile)) {
-                    resourceId = field.getInt(null);
-                }
-            }   catch (IllegalAccessException iae)  {
-                Log.e("Error", iae.toString());
-            }
-        }
+        resourceId = getResources().getIdentifier(audioFile, "raw", "com.daac.phrases");
 
         if(resourceId != -1)    {
-            MediaPlayer.create(this, resourceId);
+            MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+            mediaPlayer.start();
         }   else    {
             Log.e("Error", "File not found: " + audioFile);
         }
